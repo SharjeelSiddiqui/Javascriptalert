ca49360f81b2c16d9e1ba73f2ae6b8fe06e7254f
Sherjeel Siddiqui <sharjeelsiddiqui.fk@gmail.com>
2016-10-03 02:21:42 +0500

    using alert

---
+++ alert.js
@@ -0,0 +1,20 @@
+alert ("you looks like 420, rush from here shahbash,, 9211 hojao");
+     var a = 4;
+	 var b = 23;
+	 var m = "20";
+	 var ejaz = 4;
+	 var v = "11";
+	 var c =(a+m+ejaz*b+v);
+	 alert (c);
+	 alert ("please check the Console Log.");
+	 console.log("Do not mind please, it was just kidding.");
+     
+	 var sharjeel = confirm ("Are you Sharjeel Siddiqui?") ;
+	 
+	 if (sharjeel==true) {
+		 open ("http://www.facebook.com") ;
+	 }
+	 else {
+		 alert ("the value is <u> false </u>. you pressed <b> CANCEL </b> button.") ;
+	 }
+	
